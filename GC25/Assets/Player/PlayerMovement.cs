f1d00508af20396c73c2d79982b9ecd53d2204a5
Konsta Karapalo <konsta.karapalo@gmail.com>
2025-07-23 14:56:52 +0300

    Speed boost power-up & power-up slider colors
    - Added speed boost power-up functionality to player
    - Implemented separate UI slider for speed boost duration
    - Assigned unique colors to bomb and speed power-up sliders

--- GC25/Assets/Player/PlayerMovement.cs
+++ GC25/Assets/Player/PlayerMovement.cs
@@ -1,4 +1,6 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -7,6 +9,8 @@ public class PlayerMovement : MonoBehaviour
 
     private Vector2 moveInput = Vector2.zero;
     private bool gameStarted = false;
+
+    private Coroutine speedBoostCoroutine;
 
     public void SetMoveInput(Vector2 input)
     {
@@ -24,5 +28,42 @@ public class PlayerMovement : MonoBehaviour
 
         Vector3 movement = new Vector3(moveInput.x, moveInput.y, 0f);
         transform.position += movement * moveSpeed * Time.deltaTime;
+    }
+
+    public void ApplySpeedBoost(float multiplier, float duration, Slider slider, Color sliderColor)
+    {
+        if (speedBoostCoroutine != null)
+            StopCoroutine(speedBoostCoroutine);
+
+        speedBoostCoroutine = StartCoroutine(SpeedBoostCoroutine(multiplier, duration, slider, sliderColor));
+    }
+
+    private IEnumerator SpeedBoostCoroutine(float multiplier, float duration, Slider slider, Color sliderColor)
+    {
+        float originalSpeed = moveSpeed;
+        moveSpeed *= multiplier;
+
+        if (slider != null)
+        {
+            slider.gameObject.SetActive(true);
+            slider.fillRect.GetComponent<Image>().color = sliderColor;
+            slider.value = 1f;
+        }
+
+        float timeLeft = duration;
+        while (timeLeft > 0f)
+        {
+            timeLeft -= Time.deltaTime;
+
+            if (slider != null)
+                slider.value = timeLeft / duration;
+
+            yield return null;
+        }
+
+        moveSpeed = originalSpeed;
+
+        if (slider != null)
+            slider.gameObject.SetActive(false);
     }
 }
